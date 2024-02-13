c063deefe0eedb113c242fba89d761596b6dd257
Steve Hong <stevehonggg@gmail.com>
2024-02-13 14:31:47 +0900

    Initial Commit

---
+++ SandboxVisionOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SandboxVisionOS
+//
+//  Created by 홍성범 on 2/13/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ SandboxVisionOS/SandboxVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxVisionOSApp.swift
+//  SandboxVisionOS
+//
+//  Created by 홍성범 on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SandboxVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
