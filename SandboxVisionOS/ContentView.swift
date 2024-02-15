c2425bffff26e952dcc92b3025dadb48ee7c6198
Steve Hong <stevehonggg@gmail.com>
2024-02-15 19:36:58 +0900

    Experience Reality Composer Pro

--- SandboxVisionOS/ContentView.swift
+++ SandboxVisionOS/ContentView.swift
@@ -10,17 +10,39 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
+    
+    @State private var birthRate: Float = 100
+    @State private var width: Float = 0.1
+
+    
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
+            RealityView { content in
+                if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
+                    content.add(scene)
+                }
+            } update: { content in
+                guard let scene = content.entities.first else { return }
+                let particleEntity = scene.findEntity(named: "Magic")
+                guard var particles = particleEntity?.components[ParticleEmitterComponent.self] else { return }
+                
+                particles.mainEmitter.birthRate = birthRate
+                particles.emitterShapeSize = [width, particles.emitterShapeSize.y, particles.emitterShapeSize.z]
 
-            Text("Hello, world!")
+                particleEntity?.components.set(particles)
+            }
+            
+            LabeledContent("Birth rate") {
+                Slider(value: $birthRate, in: 1...1000)
+            }
+            
+            LabeledContent("Width") {
+                Slider(value: $width, in: 0...5)
+            }
         }
-        .padding()
     }
 }
 
-#Preview(windowStyle: .automatic) {
+#Preview {
     ContentView()
 }
